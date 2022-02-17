7daea86a88848d06b0a24ef745f67d7bb7016bf2
lulo93 <94081621+lulo93@users.noreply.github.com>
2022-02-17 22:23:48 +0000

    added container to align wrap all elements

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer">
-        <small>
-          <a
-            href="https://github.com/lulo93/dictionary-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open Source
-          </a>{" "}
-          code by Luisa Rua Estrada
-        </small>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello world</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/lulo93/dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open Source
+            </a>{" "}
+            code by Luisa Rua Estrada
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
