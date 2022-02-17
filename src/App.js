e6952fd07ffc1fc1b9ff83240adc5a8dcfc997ef
lulo93 <94081621+lulo93@users.noreply.github.com>
2022-02-17 22:17:42 +0000

    added a new component with the dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,21 @@ function App() {
       <header className="App-header">
         <h1>Hello world</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+          <a
+            href="https://github.com/lulo93/dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Source
+          </a>{" "}
+          code by Luisa Rua Estrada
+        </small>
+      </footer>
     </div>
   );
 }
